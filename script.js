46fe2e6229e9bbf69d03cc0c8dc4cf4594ba680f
Sebastian Markvad Vind <seba7412@edu.campusvejle.dk>
2021-01-07 14:33:23 +0100

    lidt vue

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return {
             mainpage: true,
             notes: [],
+            input: {},
         };
     },
 
@@ -13,9 +14,59 @@ const app = Vue.createApp({
         mainPageFalse() {
             this.mainpage = false;
         },
-        aa() {
-            // console.log(" aaaaaayyy  " + this.notes.length
-            // return true;
+
+        // inputs
+        saveInput() {
+            this.input.date = Date.parse(new Date);
+            this.notes.push(Object.assign({}, this.input));
+            this.mainPageTrue();
+            localStorage.setItem("moodAppNotes", this.notes);
+
+            // test
+            console.log(this.input)
+            console.log(this.notes)
+            console.log(this.noNotes)
+            console.log(this.notes.length)
+        },
+        titleInput(event) {
+            this.input.title = event.target.value;
+        },
+        moodInput(moodNum) {
+            this.input.mood = moodNum;
+        },
+        descriptionInput(event) {
+            this.input.description = event.target.value;
+        },
+
+        // emoji color
+        emojiColor(emojiNum) {
+            if (emojiNum === this.input.mood) {
+                return "#ffff00";
+            }
+        },
+
+        // test
+        test(date) {
+            console.log(Date.parse(new Date))
+
+            // return Math.round(Date.parse(new Date) - date)
+            return Date.parse(new Date) - date
+        },
+
+        load() {
+            this.notes = localStorage.getItem("moodAppNotes");
+            console.log(localStorage.getItem("moodAppNotes"))
+        }
+    },
+
+    computed: {
+        noNotes() {
+            if (this.notes.length > 0) {
+                return false;
+            }
+            else {
+                return true;
+             }
         }
     },
 });
